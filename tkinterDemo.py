af1b3cad03d3568fd56027e5cee630193ec801d0
CrashInfinite <shaunpreston1991@gmail.com>
2013-03-29 19:59:00 +0000

    changed tab size.

--- tkinterDemo.py
+++ tkinterDemo.py
@@ -10,11 +10,11 @@ window = w.create_rectangle(150,295,200,305, fill='green')
 window_state = 0
 
 def switch(state):
-	print state
-	if state == 0:
-		state = 1
-	else:
-		state = 0
+    print state
+    if state == 0:
+    	state = 1
+    else:
+    	state = 0
 
 b = Button(root, text='switch', command=switch(window_state))
 b.pack()
