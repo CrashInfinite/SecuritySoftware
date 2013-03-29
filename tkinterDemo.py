886588471d7213ccdd6186edc793f91b447aa0d9
CrashInfinite <shaunpreston1991@gmail.com>
2013-03-29 22:48:10 +0000

    updated code, added building functionality

--- tkinterDemo.py
+++ tkinterDemo.py
@@ -5,7 +5,21 @@ root = Tk()
 w = Canvas(root, width=500, height=500)
 w.pack()
 
-room = w.create_rectangle(50,50,300,300)
+class Room(object):
+    def __init__(self, name, pos=None, sensors=None):
+        self.name = name
+        if pos == None:
+            self.pos = []
+        else:
+            self.pos = pos
+        if sensors == None:
+            self.sensors = []
+        else:
+            self.sensors = sensors
+        w.create_rectangle(self.pos[0],self.pos[1], self.pos[2], self.pos[3])
+
+    def addSensor(self, sensor):
+        self.sensors.append(sensor)
 
 class Sensor(object):
     def __init__(self, name, status, pos=None):
@@ -22,7 +36,7 @@ class Sensor(object):
             self.status = 'CLOSED'
         else:
             self.status = 'OPEN'
-        print self.name + ' - ' + self.status
+        print '{} - {}'.format(self.name, self.status)
 
     def checkStatus(self):
         root.after(200, lambda : self.checkStatus())
@@ -31,18 +45,24 @@ class Sensor(object):
         else:
             w.itemconfig(self.img, fill='green')
 
+building = []
+
+office = Room('Office', [50,50,300,300], [])
+building.append(office)
+
 window = Sensor('win0', 'OPEN', [170,295,200,305])
-door = Sensor('door0', 'CLOSED', [295,150,305,180])
-sensors = []
-sensors.append(window)
-sensors.append(door)
+door = Sensor('door0', 'OPEN', [295,150,305,180])
+
+office.addSensor(window)
+office.addSensor(door)
+
 
 b = Button(root, text='Unlock Door', command=lambda : door.flipStatus())
 b.pack()
 c = Button(root, text='Unlock Window', command=lambda : window.flipStatus())
 c.pack()
 
-for item in sensors:
+for item in office.sensors:
     root.after(200, item.checkStatus())
 
 mainloop()
