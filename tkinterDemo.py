76c99666d4646f12303aaebb0d5b111a2ca9dcbe
CrashInfinite <shaunpreston1991@gmail.com>
2013-03-31 18:44:37 +0100

    testing colour, addition of sensors/rooms and system class

--- tkinterDemo.py
+++ tkinterDemo.py
@@ -22,15 +22,13 @@ class Room(object):
         
     def draw_room(self):
         for x in range(len(self.corners)-1):
-            w.create_line(self.corners[x], self.corners[x+1])
+            w.create_line(self.corners[x], self.corners[x+1], fill='white' )
 
     def add_sensor(self, sensor):
         self.sensors.append(sensor)
 
     def room_data(self):
-        output = 'Room: {} \n'.format(self.name)
-        output += '\n'.join(sensor.sensor_data() for sensor in self.sensors)
-        return output
+        return '\n'.join(sensor.sensor_data() for sensor in self.sensors)
 
 ##############################################################################
 
@@ -44,8 +42,8 @@ class Sensor(object):
         self.pos = pos
         
     def draw_sensor(self,):
-        i = w.create_rectangle(
-            self.pos[0],self.pos[1]-2,self.pos[0]+25,self.pos[1]+2)
+        i = w.create_oval(
+            self.pos[0]-3,self.pos[1]-3,self.pos[0]+3,self.pos[1]+3)
 
         if self.status == 'OPEN':
             w.itemconfig(i, fill='red')
@@ -57,7 +55,7 @@ class Sensor(object):
             self.status = 'CLOSED'
         else:
             self.status = 'OPEN'
-        l.config(text=office.room_data()) 
+        l.config(text=chapel.return_data()) 
         #only refers to one room, need to make universal
 
     def check_status(self):
@@ -68,7 +66,7 @@ class Sensor(object):
             self.draw_sensor()
 
     def sensor_data(self):
-        return 'Sensor {} is currently {}'.format(self.name, self.status)
+        return 'Sensor {} - {}'.format(self.name, self.status)
 
 ##############################################################################
 
@@ -88,40 +86,58 @@ class System(object):
         sensor.draw_sensor()
         room.add_sensor(sensor)
 
+    def return_data(self):
+        output = ''
+        for room in self.rooms:
+            output += 'Room: {} \n-------------\n'.format(room.name)
+            output += '\n'.join(sensor.sensor_data() for sensor in room.sensors)
+            output += '\n\n'
+        return output
+
+
 ##############################################################################
 
 #creating GUI elements
 root = Tk()
 
-w = Canvas(root, width=500, height=500)
+w = Canvas(root, width=600, height=600, bg='black')
 w.pack()
 
 f = Frame(root)
-f.pack(side=BOTTOM)
+f.pack()
 
-b = Button(f, text='Unlock Door', command=lambda : sensor1.flip_status())
+b = Button(f, text='Trip Office Door', command=lambda : sensor1.flip_status())
 b.pack(side=LEFT)
-c = Button(f, text='Unlock Window', command=lambda : sensor2.flip_status())
+c = Button(f, text='Trip Office Window', command=lambda : sensor2.flip_status())
+c.pack(side=LEFT)
+c = Button(f, text='Trip Bedroom Window', command=lambda : sensor3.flip_status())
 c.pack(side=LEFT)
 
-l = Label(root, text='awaiting input', justify=LEFT)
-l.pack(side=BOTTOM)
+l = Label(root, text='awaiting input', justify=LEFT, bg='black', fg='white')
+l.place(x=400, y=100)
 
 ##############################################################################
 
 #creating the actual layout and sensors
 chapel = System('chapel')
 
-corners = [(100,100),(300,100),(350,130),(350,230),
-            (300,250),(100,250),(100,100)]
+office_corners = [(50,100),(250,100),(300,130),(300,230),
+            (250,250),(50,250),(50,100)]
 
-office = Room('office', corners)
-sensor1 = Sensor('win0', 'OPEN', [250,250])
-sensor2 = Sensor('door0', 'CLOSED', [150,100])
+bedroom_corners = [(50,250), (120,250), (120,350), (110,370),
+            (110,370), (70,370), (50,350), (50,250)]
+
+office = Room('office', office_corners)
+bedroom = Room('bedroom', bedroom_corners)
+sensor1 = Sensor('win0', 'OPEN', [200,250])
+sensor2 = Sensor('door0', 'CLOSED', [100,100])
+sensor3 = Sensor('win1', 'OPEN', [120,300])
 
 chapel.insert_room(office)
+chapel.insert_room(bedroom)
 chapel.insert_sensor(office, sensor1)
 chapel.insert_sensor(office, sensor2)
+chapel.insert_sensor(bedroom, sensor3)
 
 #poll for changes to the status of all sensors
 for room in chapel.rooms:
